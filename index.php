8199ab7ab6df7973ca736f4e33f891fff39ed6b1
Renato Guerra <renato@ebs.edu.br>
2015-01-13 17:04:31 -0200

    to do

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 /**
- * todo Fazer uma lista to do.
+ * todo - Fazer uma página de login;
+ * todo - Fazer o mecanismo de login;
+ * todo - Criar a home restrita com listagem das páginas;
+ * todo - Conectar as páginas a um editor online;
+ * todo - Configurar o update automático ao banco de dados;
+ * todo - Configurar o redirecionamento;
+ * todo - Configurar usuário e senha seguros;
+ * todo - Criar fixture para adicionar usuário e senha;
 
 $route = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 $path = explode('/', substr($route['path'], 1));
