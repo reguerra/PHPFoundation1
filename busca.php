1f14ac09031748a75d4389dec6b78531129bc3f8
Renato Guerra <renato@ebs.edu.br>
2015-01-13 11:19:46 -0200

    comittando a busca

--- busca.php
+++ busca.php
@@ -4,32 +4,23 @@ require_once "conexao_bd.php";
 
 require_once "header.php";
 
-$busca = $_GET['busca'];
-$busca = mysql_real_escape_string($busca);
-$search = "SELECT conteudo FROM conteudo WHERE conteudo LIKE '%$busca%' OR titulo LIKE '%$busca%';";
-$resultado = conexaoDB()->query($search);
-$links = $resultado->fetch(PDO::FETCH_ASSOC);
+$busca = filter_input($_GET, "busca");
+$search = "SELECT * FROM conteudo WHERE conteudo LIKE :busca OR titulo LIKE :busca";
+$stmt = conexaoDB()->prepare($search);
+$busca = "%{$busca}%";
+$stmt->bindParam(":busca", $busca);
+$stmt->execute();
+$resultado = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
-
-foreach ($links as $res) {
-    $link = "/{$res['titulo']}";
-    $titulo = $res['titulo'];
-    echo "<a href=".$link.">".$titulo;
+if (count($resultado)) {
+    foreach($resultado as $res) {
+        echo "<ul>";
+        echo "<li>";
+        echo "<a href=\"{$res['titulo']}\">{$res['titulo']}</a>";
+        echo "</li>";
+    }
+    echo "</ul>";
 }
 
-/*echo "<ul>";
-//while ($resultado = mysql_fetch_assoc($busca)) {
 
-foreach ($resultado as $res) {
-    $titulo = $res['titulo'];
-    $texto = $res['conteudo'];
-    $link = "/{$res['titulo']}";
-    echo "<li>";
-echo "<a href=".$link." title=".$titulo.">".$titulo."</a><br>";
-//echo "<p>".$texto."</p>";
-//echo "<a href=".$link." title=".$titulo.">".$link."</a>";
-echo "</li>";
-}
-echo "</ul>";
-
-require_once "footer.php";*/
+require_once "footer.php";
